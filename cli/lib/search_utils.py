2ff483ad51e767a88e7cd2f8b4fc08699f68265a
Yahia Hamitou <hamitou.yah@gmail.com>
2026-04-24 00:46:29 +0200

    Add stopwords file loading to search utilities

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,10 +1,17 @@
 import json
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/"data"/"movies.json"
+PROJECT_ROOT      = Path(__file__).resolve().parents[2]
+DATA_PATH         = PROJECT_ROOT/"data"
+MOVIES_PATH       = DATA_PATH/"movies.json"
+STOPWORDS_PATH    = DATA_PATH/"stopwords.txt"
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, "r") as f:
+    with open(MOVIES_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stopwords():
+    with open(STOPWORDS_PATH, "r") as f:
+        data = f.read().splitlines()
+    return data
