1c156ab683af32bac8749f4271542dd03d1a1a3e
Yahia Hamitou <hamitou.yah@gmail.com>
2026-04-24 00:45:54 +0200

    Add tokenization with stemming and stopword filtering to keyword search

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,11 +1,44 @@
-from lib.search_utils import load_movies
+import string
+from nltk.stem import PorterStemmer
+from .search_utils import load_movies, load_stopwords
+
+stemmer = PorterStemmer()
+
+def clean_text(text):
+    text = text.lower() 
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+def tokenize_text(text):
+    text      = clean_text(text)
+    stopwords = load_stopwords()
+    result    = []
+    def __filter(token):
+        token = token.strip("\n")
+        if token and token not in stopwords:
+            return True
+        return False 
+    for token in text.split():
+        if __filter(token):
+            token = stemmer.stem(token)
+            result.append(token)
+    return result
+
+def has_matching_token(query_tokens, movie_tokens):
+    for query_token in query_tokens:
+        for movie_token in movie_tokens:
+            if query_token in movie_token:
+                return True
+    return False
 
 def search_command(query, n_results):
     movies = load_movies()
-    res = []
+    result = []
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query in movie["title"]:
-            res.append(movie)
-        if len(res) == n_results:
+        movie_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, movie_tokens):
+            result.append(movie)
+        if len(result) == n_results:
             break
-    return res
+    return result
