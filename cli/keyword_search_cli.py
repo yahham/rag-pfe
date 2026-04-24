e93754bc872f5fb24c2d90e20340a446995bd87b
Yahia Hamitou <hamitou.yah@gmail.com>
2026-04-25 00:40:28 +0200

    Add build command to keyword search CLI

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.keyword_search import search_command
+from lib.keyword_search import search_command, build_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search")
@@ -10,6 +10,7 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    subparsers.add_parser("build", help="Build the inverted index")
     args = parser.parse_args()
 
     match args.command:
@@ -18,6 +19,8 @@ def main() -> None:
             results = search_command(args.query, 5)
             for i, result in enumerate(results):
                 print(f"{i}. {result['title']}") 
+        case "build":
+            build_command()
         case _:
             parser.print_help()
 
